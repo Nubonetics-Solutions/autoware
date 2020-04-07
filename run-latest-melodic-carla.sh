b73172e12dc3a3c94582e3bd3c637ac9cc0fd789
Behzad Samadi <bsamadi@nubonetics.com>
2020-04-06 21:42:18 -0400

    Adding run latest scripts

---
+++ run-latest-melodic-carla.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Autoware options
+IMAGE_NAME="nubonetics/autoware"
+AUTOWARE_TAG=${AUTOWARE_TAG:-latest}
+ROS_DISTRO=${ROS_DISTRO:-melodic}
+USER_ID=${USER_ID:-"$(id -u)"}
+SUFFIX="-carla"
+IMAGE=$IMAGE_NAME:$AUTOWARE_TAG-$ROS_DISTRO$SUFFIX
+
+#RUNTIME="--runtime=nvidia"
+RUNTIME="--gpus all"
+
+XSOCK=/tmp/.X11-unix
+XAUTH=$HOME/.Xauthority
+
+SHARED_DOCKER_DIR=/home/autoware/shared_dir
+SHARED_HOST_DIR=/home/$USER/shared_dir
+
+VOLUMES="--volume=$XSOCK:$XSOCK:rw
+         --volume=$XAUTH:$XAUTH:rw
+         --volume=$SHARED_HOST_DIR:$SHARED_DOCKER_DIR:rw"
+
+# Run Autoware
+echo "Running ${IMAGE}"
+docker run \
+    -it --rm \
+    $VOLUMES \
+    --env="XAUTHORITY=${XAUTH}" \
+    --env="DISPLAY=${DISPLAY}" \
+    --env="USER_ID=$USER_ID" \
+    --privileged \
+    --net=host \
+    $RUNTIME \
+    $IMAGE \
+    /bin/bash
